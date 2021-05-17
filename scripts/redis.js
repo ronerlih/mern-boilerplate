6a3e67f2adbcec3ccc156628cd060bcf1b511a06
ron erlih <ronerlih@gmail.com>
2021-05-16 21:31:12 -0400

    mongo atlas

--- scripts/redis.js
+++ scripts/redis.js
@@ -1,15 +1,12 @@
 // connect to Redis
 import redis from 'redis';
 const REDIS_URL = process.env.HEROKU_REDIS_CYAN_URL ;
-const redisPassword = process.env.HEROKU_REDIS_CYAN_PASS;
-const client = (REDIS_URL && redisPassword) 
-  ?   redis.createClient({
-      host: REDIS_URL,
-      user: 'h' ,
-      port: REDIS_URL.split(":").slice(-1),
-      no_ready_check: true,
-      auth_pass: redisPassword
-    })
+const client = REDIS_URL 
+  ?   redis.createClient(REDIS_URL,
+      {
+         no_ready_check: true,
+         auth_pass: redisPassword
+      })
   : redis.createClient();
 client.on('connect', () => {
   console.log(`connected to redis`);
